1fda3ff2d79c511b0e6611261cb263af3f64b8cb
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-17 20:36:02 -0400

    Init Function for data fetching

---
+++ MealMap/Functions/Functions.swift
@@ -0,0 +1,24 @@
+//
+//  Functions.swift
+//  MealMap
+//
+//  Created by Jabir Chowdhury on 5/17/23.
+//
+
+import Foundation
+func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
+  DispatchQueue.main.asyncAfter(
+    deadline: .now() + seconds,
+    execute: run)
+}
+
+
+let dataFetchFailedNotification = Notification.Name(
+  rawValue: "DataFetchFailedNotification")
+
+func fatalCoreDataError(_ error: Error) {
+print("*** Fatal error: \(error)")
+  NotificationCenter.default.post(
+    name: dataFetchFailedNotification,
+    object: nil)
+}
