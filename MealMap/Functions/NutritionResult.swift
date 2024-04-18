d8cff6bb9a934e43e407f55bbf7eafc011e80cbf
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-18 17:07:04 -0400

    Init class for nutrition

---
+++ MealMap/Functions/NutritionResult.swift
@@ -0,0 +1,14 @@
+//
+//  NutritionResult.swift
+//  MealMap
+//
+//  Created by Jabir Chowdhury on 5/18/23.
+//
+
+import UIKit
+import Foundation
+
+class NutritionResultArray: Decodable {
+    var calories: Float
+    var yield: Int
+}
