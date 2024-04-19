e20b1ef27592d979b963e7d0d46c3c1fe48a8595
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-19 09:26:54 -0400

    init String helper function

--- MealMap/Functions/String+SplitArray.swift
+++ MealMap/Functions/String+SplitArray.swift
@@ -9,5 +9,15 @@ import Foundation
 
 
 extension String {
-
+    func split() -> [String]{
+            return self.components(separatedBy: "#")
+    }
+    
+    func generalSplit() -> [String]{
+        var splitByComma = self.components(separatedBy: ",")
+        for (ind, element) in splitByComma.enumerated() {
+            splitByComma[ind] = element.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        return splitByComma
+    }
 }
