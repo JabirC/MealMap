cfd9af1880675115b6af5bb2337ed3af80445216
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-22 04:18:42 -0400

    Remove Unneccessary line

--- MealMap/View Controllers/CalendarViewController.swift
+++ MealMap/View Controllers/CalendarViewController.swift
@@ -2,7 +2,7 @@
 //  CalendarViewController.swift
 //  MealMap
 //
-//  Created by Jabir Chowdhury on 5/10/23.
+//  Created by Jabir Chowdhury
 //
 
 // UICalendarview setup inspired by ohmyswift.com/blog/2022/06/12/implementing-a-custom-native-calendar-using-uicalendarview-in-ios16-and-swift/
