f51794c0ad942c51306778a51f156f0dde2b4453
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-22 04:19:42 -0400

    implement view did load for DatePicker

--- MealMap/View Controllers/DatePickerViewController.swift
+++ MealMap/View Controllers/DatePickerViewController.swift
@@ -13,7 +13,33 @@ import CoreData
 
 class DatePickerViewController: UIViewController {
 
-  
+    var recipe: SearchResult!
+    var dateSelected: Date!
+    var managedObjectContext: NSManagedObjectContext!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        doneBarButton.isEnabled = false
+        let calendarView = UICalendarView()
+        calendarView.calendar = Calendar(identifier: .gregorian)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendarView)
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            calendarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            calendarView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        calendarView.tintColor = .systemMint
+        
+        let selection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = selection
+        print(recipe.name)
+        // Do any additional setup after loading the view.
+    }
+    
+    
 }
 
 
