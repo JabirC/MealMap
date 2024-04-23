5b90e1db2fb42caa4821c358815e65fe94283d30
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-23 02:41:15 -0400

    Implement Var LazyLoad

--- MealMap/View Controllers/CalendarRecipeViewController.swift
+++ MealMap/View Controllers/CalendarRecipeViewController.swift
@@ -9,6 +9,39 @@ import UIKit
 import CoreData
 
 class CalendarRecipeViewController: UIViewController{
+    var dateValue: Date!
+    var managedObjectContext: NSManagedObjectContext!
+    var downloadTask: URLSessionDownloadTask?
+    var changed = false
+    
+    @IBOutlet weak var dayDate: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Meal> = {
+      let fetchRequest = NSFetchRequest<Meal>()
 
+      let entity = Meal.entity()
+      fetchRequest.entity = entity
+
+      let sort = NSSortDescriptor(key: "date", ascending: true)
+      fetchRequest.sortDescriptors = [sort]
+        
+        let predicate = NSPredicate(format: "date >= %@ && date < %@",Calendar.current.startOfDay(for: dateValue) as CVarArg, Calendar.current.startOfDay(for: dateValue + 86400) as CVarArg  )
+      fetchRequest.predicate = predicate
+        
+      fetchRequest.fetchBatchSize = 20
+
+      let fetchedResultsController = NSFetchedResultsController(
+        fetchRequest: fetchRequest,
+        managedObjectContext: self.managedObjectContext,
+        sectionNameKeyPath: nil,
+        cacheName: nil)
+
+      fetchedResultsController.delegate = self
+      return fetchedResultsController
+    }()
+    
+   
   }
 
